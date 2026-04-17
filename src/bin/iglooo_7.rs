c99926bc87cb4e3b131f15abdc33e15fa24709b5
Piyush Joshi <piyush.datamonk@gmail.com>
2026-04-17 11:11:09 +0530

    Some new changes

--- src/bin/iglooo_7.rs
+++ src/bin/iglooo_7.rs
@@ -1,7 +1,6 @@
+use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
 use std::fs::{File, OpenOptions};
-
-// need to import `Write` here to use create_file() function
 use std::io::{self, BufReader, BufWriter};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,12 +15,12 @@ struct Item {
     status: Status,
 }
 
-fn read_input() -> String {
+fn read_input() -> Result<String> {
     let mut user_input = String::new();
     io::stdin()
         .read_line(&mut user_input)
-        .expect("failed to read line");
-    user_input.trim().to_string()
+        .context("failed to read line")?;
+    Ok(user_input.trim().to_string())
 }
 
 // fn create_file() {
@@ -34,9 +33,9 @@ fn read_input() -> String {
 //     writeln!(writer, "{{\"message\": \"Hello\"}}").unwrap();
 // }
 
-fn add_item(null_items: &mut Vec<Item>) {
+fn add_item(null_items: &mut Vec<Item>) -> Result<()> {
     println!("enter the product you want to add");
-    let product_name = read_input().trim().to_string();
+    let product_name = read_input()?;
     let lowercase_productname = product_name.to_lowercase();
 
     if null_items
@@ -44,55 +43,40 @@ fn add_item(null_items: &mut Vec<Item>) {
         .any(|s| s.name.to_lowercase() == lowercase_productname)
     {
         println!("item already exists");
-        // return null_items;
     } else {
-        let items = Item {
+        null_items.push(Item {
             name: product_name,
             status: Status::NotBought,
-        };
-
-        null_items.push(items);
-
-        // let file = OpenOptions::new()
-        //     .write(true)
-        //     .truncate(true)
-        //     .create(true)
-        //     .open("data.json")
-        //     .expect("failed to open file");
-        // let writer = BufWriter::new(file);
-
-        // serde_json::to_writer_pretty(writer, null_items).expect("failed to write json");
-        // println!("item added");
-
-        save_data(null_items);
+        });
+        save_data(null_items)?;
         println!("item added");
-
-        // null_items
     }
+    Ok(())
 }
 
-fn remove_item(items_list: &mut Vec<Item>) {
+fn remove_item(items_list: &mut Vec<Item>) -> Result<()> {
     if items_list.is_empty() {
-        println!("there are not items")
+        println!("there are no items");
     } else {
         view_item(items_list);
         println!("enter the item name you want to remove");
-        let removed_item = read_input();
+        let removed_item = read_input()?.to_lowercase();
 
         if let Some(index) = items_list
             .iter()
             .position(|index| index.name.to_lowercase() == removed_item)
         {
             items_list.remove(index);
-            save_data(&items_list);
+            save_data(items_list)?;
             println!("item removed");
         } else {
-            println!("no intem found");
+            println!("no item found");
         }
     }
+    Ok(())
 }
 
-fn view_item(some_items: &Vec<Item>) {
+fn view_item(some_items: &[Item]) {
     if some_items.is_empty() {
         println!("there are no items, add some items to view items")
     }
@@ -105,133 +89,107 @@ fn view_item(some_items: &Vec<Item>) {
     }
 }
 
-fn buy_items(added_items: &mut Vec<Item>) {
+fn buy_items(added_items: &mut Vec<Item>) -> Result<()> {
     if added_items.is_empty() {
         println!("there are no items at this moment");
     } else {
         view_item(added_items);
         println!("enter the item name you want to buy");
-        let buying_item = read_input().trim().to_lowercase();
+        let buying_item = read_input()?.to_lowercase();
 
         if let Some(item) = added_items
             .iter_mut()
             .find(|item| item.name.to_lowercase() == buying_item)
         {
             item.status = Status::Bought;
-            save_data(&added_items);
+            save_data(added_items)?;
             println!("item marked as bought");
         } else {
-            println!("item no found");
+            println!("item not found");
         }
     }
+    Ok(())
 }
 
-fn unbought_items(bought_items: &mut Vec<Item>) {
+fn unbought_items(bought_items: &mut Vec<Item>) -> Result<()> {
     if bought_items.is_empty() {
-        println!("there are not any items");
+        println!("there are no items");
     } else {
         view_item(bought_items);
-        println!("enter the item name you want to buy");
-        let unbuying_item = read_input().trim().to_lowercase();
+        println!("enter the item name you want to unmark");
+        let unbuying_item = read_input()?.to_lowercase();
 
         if let Some(item) = bought_items
             .iter_mut()
             .find(|item| item.name.to_lowercase() == unbuying_item)
         {
             match item.status {
-                Status::NotBought => println!("already marked as bought"),
+                Status::NotBought => println!("already marked as not bought"),
                 Status::Bought => {
                     item.status = Status::NotBought;
-                    save_data(bought_items);
-                    println!("marked as unbuy");
+                    save_data(bought_items)?;
+                    println!("marked as not bought");
                 }
             }
         } else {
-            println!("item no found");
+            println!("item not found");
         }
+    }
+    Ok(())
+}
+
+fn load_data() -> Result<Vec<Item>> {
+    match File::open("data.json") {
+        Ok(file) => Ok(serde_json::from_reader(BufReader::new(file)).context("failed to parse JSON")?),
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(Vec::new()),
+        Err(e) => Err(anyhow::Error::from(e)),
     }
 }
 
-fn load_data() -> Vec<Item> {
-    let file = File::open("data.json").expect("failed to open json");
-    let reader = BufReader::new(file);
-    match serde_json::from_reader(reader) {
-        Ok(data) => {
-            let some_itemslist: Vec<Item> = data;
-            return some_itemslist;
-        }
-
-        #[allow(unused_variables)]
-        Err(e) => {
-            let some_itemslist: Vec<Item> = Vec::new();
-            return some_itemslist;
-        }
-    }
-    // some_itemslist
-}
-
-fn save_data(items_list: &Vec<Item>) {
+fn save_data(items_list: &[Item]) -> Result<()> {
     let file = OpenOptions::new()
         .write(true)
         .truncate(true)
         .create(true)
         .open("data.json")
-        .expect("failed to open file");
+        .context("failed to open file")?;
     let writer = BufWriter::new(file);
-
-    serde_json::to_writer_pretty(writer, items_list).expect("failed to write json");
+    serde_json::to_writer_pretty(writer, items_list).context("failed to write json")?;
+    Ok(())
 }
 
-fn execute(some_itemslist: &mut Vec<Item>) {
+fn execute(some_itemslist: &mut Vec<Item>) -> Result<()> {
     loop {
-        println!("\n");
+        println!("\n============================================");
+        println!("add | buy | unbuy | view | remove | exit");
         println!("============================================");
-        println!("add | buy | unbuy |view | remove | type exit");
-        println!("============================================");
 
-        // println!("type anything");
-        // let input = read_input();
-        // println!("you typed : {}", input);
-
-        // let first_item = add_item(&mut shopping_list);
-        // println!("item is : {:?}", first_item);
-
-        let new_input = read_input().trim().to_lowercase();
+        let new_input = read_input()?.to_lowercase();
         if new_input == "add" {
-            add_item(some_itemslist);
+            add_item(some_itemslist)?;
         } else if new_input == "buy" {
-            buy_items(some_itemslist);
+            buy_items(some_itemslist)?;
         } else if new_input == "view" {
             view_item(some_itemslist);
         } else if new_input == "remove" {
-            remove_item(some_itemslist);
+            remove_item(some_itemslist)?;
         } else if new_input == "unbuy" {
-            unbought_items(some_itemslist);
+            unbought_items(some_itemslist)?;
         } else if new_input == "exit" {
             break;
         } else {
             println!("invalid input");
         }
     }
+    Ok(())
 }
 
-// todo : fix reading empty file
-fn main() {
-    // let mut shopping_list: Vec<Item> = Vec::new();
-    // println!("there are some options in this which you can choose from, those options work as their name suggest");
-
-    let mut some_itemslist = load_data();
-    // println!("some thing is : {:?}", some_itemslist);
+fn main() -> Result<()> {
+    let mut some_itemslist = load_data()?;
 
     if some_itemslist.is_empty() {
-        println!("there are no items so initialized a new items list, add items to continue");
-        execute(&mut some_itemslist);
-    //     return;
-    } else {
-        execute(&mut some_itemslist);
+        println!("no items found, starting fresh");
     }
 
-    // create_file();
-    //  let some_itemslist = load_data();
-    //  println!("some thing is : {:?}", some_itemslist);
+    execute(&mut some_itemslist)
 }
